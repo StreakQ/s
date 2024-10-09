71a7c8bb29273148ff820ff553199f07682f5e01
grey_notebook <streakqqq@gmail.com>
2024-10-09 19:19:17 +0300

    2

--- work_GUI.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import (QApplication, QWidget, QVBoxLayout, QPushButton, QInputDialog,
-                             QTableWidgetItem, QTableWidget, QMessageBox, QMenu)
-from PyQt6.QtSql import *
-from PyQt6 import QtWidgets, QtCore, uic
-import re
-import sqlite3
-
-db_name = 'databases//database.db'
-
